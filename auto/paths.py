daa33210158c4c0a0b9016f6ad95f90dc105982d
kkhh <67979232+kk-pro@users.noreply.github.com>
2020-08-23 00:42:44 +0300

    compelet

---
+++ auto/paths.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/python3
+
+# usage : python3 paths.py "path"
+# example : python3 paths.py ../../
+
+import os, sys
+
+startpath = sys.argv[1]
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+list_files(startpath)
